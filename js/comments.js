9211f82209e7e633e042295427ed4db1739a1049
Abdullah Al Ashik <abdullahashik17@gmail.com>
2024-09-08 01:29:52 +0600

    async and await

---
+++ js/comments.js
@@ -0,0 +1,20 @@
+const loadData = () => {
+    fetch('https://jsonplaceholder.typicode.com/comments')
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(error => console.error('error occured', error)
+        )
+}
+
+
+const loadData2 = async () => {
+    try {
+        const res = await fetch('https://jsonplaceholder.typicode.com/comments')
+        const data = await res.json();
+        console.log(data);
+    }
+    catch(error){
+        console.error('load data error');
+        
+    }
+}
